09ebaa8a828ac7afd223ffd17a9fb565eb2f395e
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-20 22:37:41 -0300

    Add src/ReplicatedStorage/Characters/CharacterDefinitions.lua

---
+++ src/ReplicatedStorage/Characters/CharacterDefinitions.lua
@@ -0,0 +1,20 @@
+return {
+    Yuji = {
+        Id = "Yuji",
+        Name = "Yuji Itadori",
+        Subtitle = "Black Flash Momentum",
+        MaxCE = 100
+    },
+    Gojo = {
+        Id = "Gojo",
+        Name = "Satoru Gojo",
+        Subtitle = "Infinity Management",
+        MaxCE = 110
+    },
+    Sukuna = {
+        Id = "Sukuna",
+        Name = "Ryomen Sukuna",
+        Subtitle = "Slash Adaptation",
+        MaxCE = 120
+    }
+}
