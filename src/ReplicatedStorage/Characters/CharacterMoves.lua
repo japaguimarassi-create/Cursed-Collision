e98144e69f2d2106f78858b3c983dfa2015e0c27
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-21 21:23:43 -0300

    feat: add character-specific move labels

---
+++ src/ReplicatedStorage/Characters/CharacterMoves.lua
@@ -0,0 +1,28 @@
+local Moves = {
+    Yuji = {SpecialName="Divergent Fist", SkillName="Black Flash"},
+    Gojo = {SpecialName="Lapse Blue", SkillName="Limitless Shift"},
+    Sukuna = {SpecialName="Cursed Slash", SkillName="Shrine Stance"},
+    Megumi = {SpecialName="Shikigami Assault", SkillName="Ten Shadows"},
+    Yuta = {SpecialName="Rika Sword", SkillName="Copy / Rika"},
+    Maki = {SpecialName="Tool Strike", SkillName="Arsenal Shift"},
+    Toji = {SpecialName="Assassin Strike", SkillName="Arsenal Shift"},
+    Mahito = {SpecialName="Idle Transfiguration", SkillName="Body Morph"},
+    Todo = {SpecialName="Boogie Woogie", SkillName="Clap / Switch"},
+    Hakari = {SpecialName="Private Pure Love", SkillName="Jackpot Roll"},
+    Choso = {SpecialName="Piercing Blood", SkillName="Blood Burst"},
+    Kashimo = {SpecialName="Electrified Strike", SkillName="Charge Burst"},
+    Naoya = {SpecialName="Projection Strike", SkillName="Frame Step"},
+    Kenjaku = {SpecialName="Cursed Spirit Burst", SkillName="Technique Stock"},
+    Jogo = {SpecialName="Volcanic Burst", SkillName="Ember"},
+    Dagon = {SpecialName="Death Swarm", SkillName="Water Shikigami"},
+    Hanami = {SpecialName="Disaster Plants", SkillName="Root Grab"},
+    Higuruma = {SpecialName="Judgment Strike", SkillName="Evidence"},
+    Takaba = {SpecialName="Comedian", SkillName="Reality Twist"},
+    Uraume = {SpecialName="Ice Formation", SkillName="Frost Calamity"},
+    Yorozu = {SpecialName="Liquid Metal", SkillName="Construction Armor"},
+    Ryu = {SpecialName="Granite Shot", SkillName="Maximum Charge"},
+    Uro = {SpecialName="Sky Strike", SkillName="Sky Distortion"},
+    Kusakabe = {SpecialName="New Shadow Slash", SkillName="Simple Domain"}
+}
+
+return Moves
