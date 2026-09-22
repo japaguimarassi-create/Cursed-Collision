136665b9bd8f02ba76f0452f75ed89c456129b24
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-22 20:24:19 -0300

    characters: add Toji through Naoya adapters

--- src/ReplicatedStorage/Characters/Kashimo.lua
+++ src/ReplicatedStorage/Characters/Kashimo.lua
@@ -1,2 +1,6 @@
-local Factory = require(game:GetService("ReplicatedStorage").Characters.CharacterFactory)
-return Factory.Build("Kashimo")
+--!strict
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local CharacterKit = require(ReplicatedStorage.Characters.CharacterKit)
+
+return CharacterKit.Build("Kashimo")
