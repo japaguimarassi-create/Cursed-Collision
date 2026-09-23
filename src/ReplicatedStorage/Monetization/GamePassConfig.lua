65c1491dcf2d208c1243920bc0b795aceabfcbb4
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-22 21:13:19 -0300

    [cursed-collision] configure combat gamepasses

---
+++ src/ReplicatedStorage/Monetization/GamePassConfig.lua
@@ -0,0 +1,21 @@
+local GamePassConfig = {
+    UltimateSkin = {
+        Id = 0,
+        Name = "Ultimate Skin",
+        Attribute = "GP_UltimateSkin"
+    },
+    KillSound = {
+        Id = 0,
+        Name = "Kill Sound",
+        Attribute = "GP_KillSound"
+    },
+    InstantSkin = {
+        Id = 0,
+        Name = "Instant Skin Swap",
+        Attribute = "GP_InstantSkin"
+    },
+    KillSoundId = "",
+    KillSoundVolume = 1
+}
+
+return GamePassConfig
