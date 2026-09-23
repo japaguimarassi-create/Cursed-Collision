2a18fcdf5ece6541576b8e8209a9c379eb2ea60d
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-23 18:09:18 -0300

    [cursed-collision] make skill marker validation latency-safe

--- src/ServerScriptService/CombatCore/AbilityService.lua
+++ src/ServerScriptService/CombatCore/AbilityService.lua
@@ -2,6 +2,7 @@
 
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+local Config = require(ReplicatedStorage.Shared.Config)
 local Timeline = require(ReplicatedStorage.Combat.AbilityTimeline)
 local HitRegistry = require(ReplicatedStorage.Combat.HitRegistry)
 local StateManager = require(script.Parent.StateManager)
