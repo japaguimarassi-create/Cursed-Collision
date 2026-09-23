8cd43b5fc54464774d64e0ea17840deab2fe88b7
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-23 19:37:35 -0300

    [cursed-collision] reduce active roster to four focused characters

--- src/ReplicatedStorage/Characters/CharacterMoves.lua
+++ src/ReplicatedStorage/Characters/CharacterMoves.lua
@@ -1,29 +1,27 @@
-local Moves = {
-    PotentialMan = {SpecialName="Shadow Potential", SkillName="Shadow Jab"},
-    Yuji = {SpecialName="Divergent Fist", SkillName="Black Flash"},
-    Gojo = {SpecialName="Lapse Blue", SkillName="Limitless Shift"},
-    Sukuna = {SpecialName="Cursed Slash", SkillName="Shrine Stance"},
-    Megumi = {SpecialName="Shikigami Assault", SkillName="Ten Shadows"},
-    Yuta = {SpecialName="Rika Sword", SkillName="Copy / Rika"},
-    Maki = {SpecialName="Tool Strike", SkillName="Arsenal Shift"},
-    Toji = {SpecialName="Assassin Strike", SkillName="Arsenal Shift"},
-    Mahito = {SpecialName="Idle Transfiguration", SkillName="Body Morph"},
-    Todo = {SpecialName="Boogie Woogie", SkillName="Clap / Switch"},
-    Hakari = {SpecialName="Private Pure Love", SkillName="Jackpot Roll"},
-    Choso = {SpecialName="Piercing Blood", SkillName="Blood Burst"},
-    Kashimo = {SpecialName="Electrified Strike", SkillName="Charge Burst"},
-    Naoya = {SpecialName="Projection Strike", SkillName="Frame Step"},
-    Kenjaku = {SpecialName="Cursed Spirit Burst", SkillName="Technique Stock"},
-    Jogo = {SpecialName="Volcanic Burst", SkillName="Ember"},
-    Dagon = {SpecialName="Death Swarm", SkillName="Water Shikigami"},
-    Hanami = {SpecialName="Disaster Plants", SkillName="Root Grab"},
-    Higuruma = {SpecialName="Judgment Strike", SkillName="Evidence"},
-    Takaba = {SpecialName="Comedian", SkillName="Reality Twist"},
-    Uraume = {SpecialName="Ice Formation", SkillName="Frost Calamity"},
-    Yorozu = {SpecialName="Liquid Metal", SkillName="Construction Armor"},
-    Ryu = {SpecialName="Granite Shot", SkillName="Maximum Charge"},
-    Uro = {SpecialName="Sky Strike", SkillName="Sky Distortion"},
-    Kusakabe = {SpecialName="New Shadow Slash", SkillName="Simple Domain"}
+--!strict
+
+export type CharacterMoveNames = {
+    SpecialName: string,
+    AwakeningName: string
+}
+
+local Moves: {[string]: CharacterMoveNames} = {
+    Yuji = {
+        SpecialName = "Black Flash",
+        AwakeningName = "Divergent Soul"
+    },
+    Gojo = {
+        SpecialName = "Six Eyes",
+        AwakeningName = "Six Eyes Unleashed"
+    },
+    Sukuna = {
+        SpecialName = "Shrine",
+        AwakeningName = "King of Curses"
+    },
+    Megumi = {
+        SpecialName = "Ten Shadows",
+        AwakeningName = "Chimera Shadow Garden"
+    }
 }
 
 return Moves
