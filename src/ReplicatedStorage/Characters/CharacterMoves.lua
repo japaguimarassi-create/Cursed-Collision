65261a8935fefc8169887ff491bbd31d83a9a10e
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-23 20:44:43 -0300

    Update special labels for four character kits

--- src/ReplicatedStorage/Characters/CharacterMoves.lua
+++ src/ReplicatedStorage/Characters/CharacterMoves.lua
@@ -1,10 +1,10 @@
 --!strict
 
 local Moves = {
-    Yuji = {SpecialName = "Black Flash", AwakeningName = "Divergent Soul"},
-    Gojo = {SpecialName = "Six Eyes", AwakeningName = "Six Eyes Unleashed"},
-    Sukuna = {SpecialName = "Shrine", AwakeningName = "King of Curses"},
-    Megumi = {SpecialName = "Ten Shadows", AwakeningName = "Chimera Shadow Garden"}
+    Yuji = {SpecialName = "Black Flash", AwakeningName = "Shinjuku"},
+    Gojo = {SpecialName = "Infinity", AwakeningName = "Six Eyes Unleashed"},
+    Sukuna = {SpecialName = "Shrine / Enchain", AwakeningName = "King of Curses"},
+    Megumi = {SpecialName = "Ten Shadows", AwakeningName = "Ten Shadows / Mahoraga"}
 }
 
 return Moves
