51460be185aa5ebef7f38ba47a2f83a8af835cd6
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-23 18:20:19 -0300

    [cursed-collision] resolve Luau type diagnostics in integrated HUD and combat

--- src/StarterPlayer/StarterPlayerScripts/HUD/Emotes.lua
+++ src/StarterPlayer/StarterPlayerScripts/HUD/Emotes.lua
@@ -4,6 +4,7 @@ local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local GuiService = game:GetService("GuiService")
 
 local Util = require(script.Parent.Util)
 
