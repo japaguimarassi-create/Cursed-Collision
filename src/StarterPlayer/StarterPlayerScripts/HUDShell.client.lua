a3f16d6788b7abb083470e2e319eb8704b47aef7
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-23 19:18:50 -0300

    [cursed-collision] split HUD shell and input routing

---
+++ src/StarterPlayer/StarterPlayerScripts/HUDShell.client.lua
@@ -0,0 +1,47 @@
+--!strict
+
+local Players = game:GetService("Players")
+
+local HUDController = require(script.Parent.Controllers.HUDController)
+local HUDInputRouter = require(script.Parent.Controllers.HUDInputRouter)
+local HUDMenuRouter = require(script.Parent.Controllers.HUDMenuRouter)
+
+local player = Players.LocalPlayer
+local hud = HUDController.new()
+
+local function action(name: string)
+    player:SetAttribute("CCHUD_LastUIButton", name)
+end
+
+HUDInputRouter.Bind(hud, {
+    Action = action,
+    Dash = function()
+        action("Dash")
+    end,
+    BlockToggle = function()
+        action("BlockToggle")
+    end,
+    SprintToggle = function()
+        action("SprintToggle")
+    end,
+    CharacterMenu = function()
+        HUDMenuRouter:Character()
+    end,
+    EmoteMenu = function()
+        HUDMenuRouter:Emotes()
+    end,
+    MainMenu = function()
+        HUDMenuRouter:Menu()
+    end,
+    OwnerMenu = function()
+        HUDMenuRouter:Owner()
+    end
+})
+
+player:GetAttributeChangedSignal("IsGameOwner"):Connect(function()
+    hud:SetOwnerVisible(player:GetAttribute("IsGameOwner") == true)
+end)
+
+hud:SetOwnerVisible(player:GetAttribute("IsGameOwner") == true)
+
+return nil
