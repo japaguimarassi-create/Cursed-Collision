b2aa677e87b804876505a095a66d137b0624f49f
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-25 17:04:52 -0300

    add active client bootstrap marker

---
+++ src/CollisionBattlestar/StarterPlayer/StarterPlayerScripts/Client/Bootstrap.client.lua
@@ -0,0 +1,5 @@
+--!strict
+local Players=game:GetService("Players")
+local player=Players.LocalPlayer
+local gui=player:WaitForChild("PlayerGui")
+gui:SetAttribute("CollisionBattlestarClientReady",true)
