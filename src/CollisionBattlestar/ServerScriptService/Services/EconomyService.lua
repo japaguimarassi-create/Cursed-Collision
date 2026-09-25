0ac560c3e5770e88278041d0522e87c916970493
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-25 17:33:04 -0300

    prevent Battle Streak reward duplication

--- src/CollisionBattlestar/ServerScriptService/Services/EconomyService.lua
+++ src/CollisionBattlestar/ServerScriptService/Services/EconomyService.lua
@@ -3,10 +3,11 @@ local Players=game:GetService("Players")
 local Data=require(script.Parent.PlayerDataService)
 local S={}
 function S.Init(enemyDefeated:BindableEvent)
-	enemyDefeated.Event:Connect(function(_,userId:number,_,reward:number)
+	enemyDefeated.Event:Connect(function(model:Model,userId:number,_,reward:number)
 		if userId<=0 then return end
 		for _,p in Players:GetPlayers()do
 			if p.UserId==userId then
+				if model:GetAttribute("BattleStreakOwnerUserId") and (model:GetAttribute("BattleStreakOwnerUserId")or 0)>0 then return end
 				Data.AddCoins(p,math.max(0,math.floor(reward)))
 				return
 			end
