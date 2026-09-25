3ede580d17fe01e6cd17620078e6615d9be1f74f
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-25 16:59:29 -0300

    add Collision Battlestar client experience

---
+++ src/CollisionBattlestar/StarterPlayer/StarterPlayerScripts/Client/AudioController.client.lua
@@ -0,0 +1,2 @@
+--!strict
+local S=game:GetService("SoundService");local folder=S:FindFirstChild("CollisionAudio")or Instance.new("Folder");folder.Name="CollisionAudio";folder.Parent=S;folder:SetAttribute("SystemReady",true);folder:SetAttribute("ContentStatus","FINAL_AUDIO_ASSETS_NOT_IMPLEMENTED")
