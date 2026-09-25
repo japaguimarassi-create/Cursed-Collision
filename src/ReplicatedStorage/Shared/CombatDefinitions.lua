b13e4db48bb69d4c59aadb4b5c892fe506c18177
japaguimarassi-create <japaguimarassi@gmail.com>
2026-09-25 16:57:33 -0300

    add Collision Battlestar combat definitions

---
+++ src/ReplicatedStorage/Shared/CombatDefinitions.lua
@@ -0,0 +1,5 @@
+--!strict
+local D={}
+D.LightChain={[1]={Scale=1,Knockback=22},[2]={Scale=1.05,Knockback=25},[3]={Scale=1.15,Knockback=28},[4]={Scale=1.3,Knockback=55}}
+D.Valid={Light=true,Heavy=true,BlockStart=true,BlockEnd=true,Parry=true,Dash=true,Special=true,Overdrive=true,StyleToggle=true}
+return D
